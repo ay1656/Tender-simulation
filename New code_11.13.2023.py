1a1320ea162da8a99d9a3d1e36e3b76692de6196
ay1656 <128572375+ay1656@users.noreply.github.com>
2023-11-13 08:36:49 -0500

    Update New code_11.13.2023.py

--- New code_11.13.2023.py
+++ New code_11.13.2023.py
@@ -1,16 +1,15 @@
 import numpy as np
 import pandas as pd
 import simpy
-import random
 from scipy.stats import poisson
 
 # Set a fixed seed for reproducibility
 seed = 42
 np.random.seed(seed)
 
-def read_initial_conditions(initial_conditions.csv):
+def read_initial_conditions(initial_conditions_csv):
     # Read initial conditions from a CSV file into a DataFrame
-    initial_conditions = pd.read_csv(initial_conditions.csv, index_col=0)
+    initial_conditions = pd.read_csv(initial_conditions_csv, index_col=0)
     return initial_conditions
 
 
@@ -20,8 +19,6 @@ class VaccineProcurementSimulation:
         self.initial_conditions = initial_conditions
         self.antigens = initial_conditions.index
         self.providers = self.initialize_providers()
-
-        # Add additional simulation environment variables and resources as needed
 
     def initialize_providers(self):
         # Initialize providers with capabilities based on normal distribution
@@ -41,32 +38,31 @@ class VaccineProcurementSimulation:
        
         return providers
 
-    
     def process_demand(self, year):
          while True:
-                for antigen in self.antigens:
-                    # Calculate current inventory for every antigen
-                    previous_inventory = self.initial_conditions.loc[antigen, 'current_inventory']
-                    partial_deliveries = self.initial_conditions.loc[antigen, 'partial_deliveries']
-                    vaccines_used = self.initial_conditions.loc[antigen, 'vaccines_used']
-                    current_inventory = previous_inventory + partial_deliveries - vaccines_used
-                    # Calculate inventory of unvaccinated individuals per antigen each year
-                    previous_unvaccinated = self.initial_conditions.loc[antigen, 'unvaccinated_individuals']
-                    birth_cohort = np.random.poisson(self.initial_conditions.loc[antigen, 'average_births_per_year'])
-                    unvaccinated_individuals = previous_unvaccinated + birth_cohort + vaccines_used
-                    # Calculate demand for every antigen
-                    doses_required = self.initial_conditions.loc[antigen, 'doses_required']
-                    demand = unvaccinated_individuals * doses_required
-                    # Calculate unvaccinated cost per antigen
-                    unvaccinated_cost = unvaccinated_individuals * self.initial_conditions.loc[antigen, 'cost_per_unvaccinated_individual']
-                    # Update relevant variables
-                    self.initial_conditions.loc[antigen, 'current_inventory'] = current_inventory
-                    self.initial_conditions.loc[antigen, 'unvaccinated_individuals'] = unvaccinated_individuals
-                    self.initial_conditions.loc[antigen, 'demand'] = demand
-                    self.initial_conditions.loc[antigen, 'unvaccinated_cost'] = unvaccinated_cost
-                    yield self.env.timeout(1)
+            for antigen in self.antigens:
+                # Calculate current inventory for every antigen
+                previous_inventory = self.initial_conditions.loc[antigen, 'current_inventory']
+                partial_deliveries = self.initial_conditions.loc[antigen, 'partial_deliveries']
+                vaccines_used = self.initial_conditions.loc[antigen, 'vaccines_used']
+                current_inventory = previous_inventory + partial_deliveries - vaccines_used
+                # Calculate inventory of unvaccinated individuals per antigen each year
+                previous_unvaccinated = self.initial_conditions.loc[antigen, 'unvaccinated_individuals']
+                birth_cohort = np.random.poisson(self.initial_conditions.loc[antigen, 'average_births_per_year'])
+                unvaccinated_individuals = previous_unvaccinated + birth_cohort + vaccines_used
+                # Calculate demand for every antigen
+                doses_required = self.initial_conditions.loc[antigen, 'doses_required']
+                demand = unvaccinated_individuals * doses_required
+                # Calculate unvaccinated cost per antigen
+                unvaccinated_cost = unvaccinated_individuals * self.initial_conditions.loc[antigen, 'cost_per_unvaccinated_individual']
+                # Update relevant variables
+                self.initial_conditions.loc[antigen, 'current_inventory'] = current_inventory
+                self.initial_conditions.loc[antigen, 'unvaccinated_individuals'] = unvaccinated_individuals
+                self.initial_conditions.loc[antigen, 'demand'] = demand
+                self.initial_conditions.loc[antigen, 'unvaccinated_cost'] = unvaccinated_cost
+                yield self.env.timeout(1)
 
-    def tender_scheduling(self, year):
+    def tender_scheduling(self, year, production_lead_time):
         while True:
             for antigen in self.antigens:
                 if self.is_tender_scheduled(antigen, year):
@@ -77,15 +73,17 @@ class VaccineProcurementSimulation:
                     # Calculate partial deliveries
                     partial_deliveries = committed_doses / self.initial_conditions.loc[antigen, 'time_space']
 
-                    # Update provider capabilities
-                    self.update_provider_capabilities(provider, committed_doses)
-
                     # Update relevant variables
                     self.initial_conditions.loc[antigen, 'tender_cost'] = tender_cost
                     self.initial_conditions.loc[antigen, 'partial_deliveries'] = partial_deliveries
 
+                    # Production lead time - wait before updating provider capabilities
+                    yield self.env.timeout(production_lead_time)
+
+                    # Update provider capabilities
+                    self.update_provider_capabilities(antigen, provider, committed_doses)
+
     def is_tender_scheduled(self, antigen, year):
-        
         reorder_point = self.initial_conditions.loc[antigen, 'reorder_point']
         time_space = self.initial_conditions.loc[antigen, 'time_space']
         provider = self.choose_provider(antigen)
@@ -112,8 +110,8 @@ class VaccineProcurementSimulation:
             # Check if the provider has enough capability to cover the committed doses
             if capability >= committed_doses:
                 # Check if the provider produces vaccines that cover the antigen
-            if self.provider_covers_antigen(provider, antigen):
-                eligible_providers.append(provider)
+                if self.provider_covers_antigen(provider, antigen):
+                    eligible_providers.append(provider)
 
         if not eligible_providers:
             # If no eligible providers are found, raise an exception or handle the situation accordingly
@@ -124,24 +122,24 @@ class VaccineProcurementSimulation:
         return chosen_provider
 
     def provider_covers_antigen(self, provider, antigen):
-    # Check if the given provider produces vaccines that cover the specified antigen
-    return provider.startswith(f'Provider{self.initial_conditions.loc[antigen, "provider_prefix"]}')
+        # Check if the given provider produces vaccines that cover the specified antigen
+        return provider.startswith(f'Provider{self.initial_conditions.loc[antigen, "provider_prefix"]}')
 
-    def update_provider_capabilities(self, provider, committed_doses):
+    def update_provider_capabilities(self, antigen, provider, committed_doses):
         # Update provider capabilities after a tender is scheduled
         # Reduce the capabilities of the selected provider
-        # Placeholder for illustration, replace with actual implementation
         self.providers[antigen][provider] -= committed_doses
-        pass
 
-    def run_simulation(self, num_years):
+    def run_simulation(self, num_years, production_lead_time):
         for antigen in self.antigens:
             # Start SimPy processes for each antigen
             self.env.process(self.process_demand(antigen))
-            self.env.process(self.tender_scheduling(antigen))
+            self.env.process(self.tender_scheduling(antigen, production_lead_time))
 
         # Run the simulation for a specified number of years
         self.env.run(until=num_years)
+
+
 def main():
     # Specify the file path for your initial conditions CSV file
     file_path = 'initial_conditions.csv'
@@ -149,13 +147,13 @@ def main():
     # Read initial conditions from the CSV file
     initial_conditions = read_initial_conditions(file_path)
     
-    env=simpy.Environment()
+    env = simpy.Environment()
 
     # Instantiate the simulation with the read initial conditions
-    simulation = VaccineProcurementSimulation(initial_conditions)
+    simulation = VaccineProcurementSimulation(env, initial_conditions)
 
-    # Run the simulation
-    simulation.run_simulation(num_years=15)
+    # Run the simulation with a production lead time of 2 time units
+    simulation.run_simulation(num_years=15, production_lead_time=2)
 
 if __name__ == "__main__":
     main()
